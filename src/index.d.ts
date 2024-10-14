e4eb1d261b0676b699e281ac7bb0ce2f0c158da9
Alex Yatkevich <ayatkevich@users.noreply.github.com>
2024-10-15 03:04:33 +0700

    bump version

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,8 @@
-import { PGlite, Results } from "@electric-sql/pglite";
+import { Results } from "@electric-sql/pglite";
 
 export interface Database {
-  pg: PGlite;
   sql: (sqlStrings: TemplateStringsArray, ...params: any[]) => Promise<Results["rows"]>;
 }
-
-export function prepareDatabase(): Database;
 
 export function parity(
   definition: (sql: Database["sql"]) => Promise<any>,
